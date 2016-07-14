f5144b3b33c0d0ca679e0e4ea4b0b4c32e021430
aacister <aacister@gmail.com>
2016-07-13 21:24:45 -0400

    Updates to Header middleware

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ var users = require('./routes/users');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+next();
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -40,6 +47,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(allowCrossDomain);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -56,12 +56,6 @@ router.param('locality', function(req, res, next, locality){
   });
 });
 
-router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
